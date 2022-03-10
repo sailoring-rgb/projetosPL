0c97d61e4b7717db0801507061c8086372a7eba0
Ana Henriques <pbapohenriques@hotmail.com>
2022-03-10 19:25:06 +0000

    função header

--- TP1/src/main.py
+++ TP1/src/main.py
@@ -1,15 +1,35 @@
 import re
 import sys
 
-header = "Número,Nome,Curso,Notas{5}::avg,,,,,Idade"
+h = "Número,Nome,Curso,Notas{5}::media,,,,,Idade,Contas{3}"
 
-columns = []
-list_or_not = []
-functions = ["sum","media","min","max","count"]
+def header(line):
 
-# elements vai buscar todos os campos do cabeçalho e devolve uma lista de tuplos, em que cada tuplo contém três valores:
-# primeiro: nome da coluna (que se encontra antes de uma vírgula ou de um valor N ou de uma função)
-# segundo: valor de N (pode ser nulo)
-# terceiro: função de agregação (pode ser nulo)
-elements = re.findall(r'([^:{},]+)(?:{(.*)})?(?:\:\:(.*?)(?:,))?', header)
-print(elements)
+	columns = []                                        # contem o nome das colunas
+	operations = []                                     # contem as funções de agreg. que serão feitas para cada campo se este corresponder a uma lista
+	functions = ["sum","media","min","max","count"]     # as funções de agreg. possíveis
+
+	"""	
+	elements vai buscar todos os campos do cabeçalho e devolve uma lista de tuplos, em que cada tuplo contém três valores:
+		- primeiro: nome da coluna (que se encontra antes de uma vírgula ou de um valor N ou de uma função)
+		- segundo: valor de N (pode ser nulo)
+		- terceiro: função de agregação (pode ser nulo)
+	"""
+	elements = re.findall(r'([^:{,]+)(?:{(.*?)})?(?:\:\:(.*?)(?:,))?', line)
+	# print(elements)
+
+	for i in elements:
+		columns.append(i[0])
+		if len(list(filter(None,i))) == 1:
+			t = (0,"none")
+			operations.append(t)
+		elif len(list(filter(None,i))) == 2:
+			t = (i[1],"none")
+			operations.append(t)
+		else:
+			t = (i[1],i[2])
+			operations.append(t)
+	# print(operations)
+	# print(columns)
+
+header(h)
