02d16dd91badabce7fd1a0784ea2b421364f0822
Ana Henriques <pbapohenriques@hotmail.com>
2022-04-16 20:32:52 +0100

    update .gitignore and delete trash

--- TP2/src/helper.py
+++ TP2/src/helper.py
@@ -24,32 +24,3 @@ def lex_or_yacc(lines: List[str]):
         lines_for_YACC.append(line)
     
     return lines_for_LEX, lines_for_YACC
-
-
-def open_file():
-    file_name = input("[CSV] Insert file name (extension included): ")
-    try:	
-        file = open("../input/"+file_name)
-    except OSError:
-        print(f"[ERROR] Can't locate CSV file \"{file_name}\".\n")
-        input("[PRESS ENTER TO CONTINUE]")
-        return -1
-
-    if file:
-        lines = file.read().splitlines()
-        print("[FILE] Opened successfully.")
-        input("[PRESS ENTER TO CONTINUE]")
-        file.close()
-        return lines
-
-lines = open_file()
-lines_for_LEX, lines_for_YACC = lex_or_yacc(lines)
-
-"""
-print("-------------------------------------FOR LEX---------------------------------------------")
-for line in lines_for_LEX:
-    print(line)
-print("-------------------------------------FOR YACC---------------------------------------------")
-for line in lines_for_YACC:
-    print(line)
-"""
--- TP2/src/main.py
+++ TP2/src/main.py
@@ -1,2 +1,28 @@
 from helper import *
 
+def open_file():
+    file_name = input("[CSV] Insert file name (extension included): ")
+    try:	
+        file = open("../input/"+file_name)
+    except OSError:
+        print(f"[ERROR] Can't locate CSV file \"{file_name}\".\n")
+        input("[PRESS ENTER TO CONTINUE]")
+        return -1
+
+    if file:
+        lines = file.read().splitlines()
+        print("[FILE] Opened successfully.")
+        input("[PRESS ENTER TO CONTINUE]")
+        file.close()
+        return lines
+
+lines = open_file()
+lines_for_LEX, lines_for_YACC = lex_or_yacc(lines)
+
+
+print("-------------------------------------FOR LEX---------------------------------------------")
+for line in lines_for_LEX:
+    print(line)
+print("-------------------------------------FOR YACC---------------------------------------------")
+for line in lines_for_YACC:
+    print(line)
