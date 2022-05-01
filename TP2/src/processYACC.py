6b09500710d65550134069d865fbc659194f1cc4
Ana Henriques <pbapohenriques@hotmail.com>
2022-05-01 13:13:05 +0100

    yacc is fully translated - DONE

--- TP2/src/processYACC.py
+++ TP2/src/processYACC.py
@@ -5,7 +5,7 @@ from typing import List
 # PROCESSA A GRAMÁTICA RECEBIDA E DEVOLVE UMA STRING COM TODAS AS FUNÇÕES PARA O FILE YACC
 def process_grammar(grammar: List[str]):
     i = 1
-    res = ""
+    res_grammar = ""
     defGrammar = ["# GRAMMAR:"]
 
     for line in grammar:
@@ -26,16 +26,16 @@ def process_grammar(grammar: List[str]):
                 defGrammar.append(f"# p{i}:      | {prod}")
             else: defGrammar.append(f"# p{i}:    {group[0]} -> {prod}")
 
-            res += f"""def p_{group[0]}_p{i}(p):
+            res_grammar += f"""def p_{group[0]}_p{i}(p):
     "{group[0]} : {prod}"
     {calc}\n\n"""
             i = i+1
     
-    return defGrammar, res
+    return defGrammar, res_grammar
 
 
 # PROCESSA UMA FUNÇÃO DESTINADA AO FILE YACC
-def process_function(lines: List[str], line: str, i: int, res: str):
+def process_function(lines: List[str], line: str, i: int, res_functions: str):
 
     function = []
     if re.match(r'def ',line):
@@ -46,11 +46,11 @@ def process_function(lines: List[str], line: str, i: int, res: str):
                function.append(s)
             else: break
             f = f + 1
-        res += "\n".join(function) + "\n\n"
+        res_functions += "\n".join(function) + "\n\n"
         i = f - 1
     else: i = i + 1
 
-    return i, res
+    return i, res_functions
 
 
 # DEVOLVE UMA LISTA COM O CONTEÚDO TRADUZIDO PARA O FILE YACC
@@ -60,20 +60,30 @@ def translate_yacc(lines: List[str]):
     res = ""
 
     grammar = content[content.index("{}") + len("{}"):content.index("%%") + len("%%")-2]      # pega nas linhas destinadas à gramática
-    defGrammar, res0 = process_grammar(grammar.split("\n"))
+    defGrammar, res_grammar = process_grammar(grammar.split("\n"))
     res += "\n".join(defGrammar) + "\n\n"                                                       # construir a gramática em comentários
 
-    grammar = content[content.index("{}") + len("{}"):content.index("%%") + len("%%")-2]
-    dictionary = (re.findall(r'.*\{\}',content))[0]
-    res += dictionary + "\n\n"
-
-    res += res0                         # escreve as funções que processam as produções da gramática
-
     i = 0
+    res_functions = ""
+    precedence = []
     for line in lines:
 
-        i, res = process_function(lines, line, i, res)
+        if re.match(r'%\w+\s?=\s?\[.*',line):
+            newline = re.sub(r'%','',line)
+            precedence.append(newline)
+            for s in lines[lines.index(line)-len(lines)+1:]:
+                if re.search(r'\s*\(.*\),|\]',s):
+                    precedence.append(s)
+                else: break
+
+        i, res_functions = process_function(lines, line, i, res_functions)
+
         parser_match = re.findall(r'([^ \.])(?:\.parse\((.*?)\))',line)       # grupo 1 - nome da var. # grupo 2 - conteúdo do parse
+
+    res += "\n".join(precedence) + "\n\n"
+
+    dictionary = (re.findall(r'.*\{\}',content))[0]
+    res += dictionary + "\n\n" + res_grammar + res_functions
 
     res += f"""{parser_match[0][0]} = yacc.yacc()
 {parser_match[0][0]}.parse({parser_match[0][1]})"""
