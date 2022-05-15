c00210ca720203792a9cab668a4e0fc73002232a
Anaa10 <a93284@alunos.uminho.pt>
2022-05-15 22:57:18 +0100

    mais cenas no lex

--- TP2/src/plySimple_lex.py
+++ TP2/src/plySimple_lex.py
@@ -1,12 +1,12 @@
 import ply.lex as lex
 
-tokens = ['LIT','TOK', 'IGNORE', 'ERROR', 'VAR']
+tokens = ['LIT','TOK', 'IGN','ERR','BLEX','RET']
 states = [("lex", 'inclusive'),
           ("yacc", 'inclusive')]
 
 t_ignore = " \t\n"
 
-def t_LEX(t):
+def t_BLEX(t):
     r'%% *LEX'
     t.lexer.begin("lex")
     return t 
@@ -29,14 +29,29 @@ def t_TOK(t):
     r'% *tokens'
     return t
 
-def t_ERROR(t):
+def t_ERR(t):
     print(f"Illegal character '{t.value[0]}', [{t.lexer.lineno}]")
     t.lexer.skip(1)
 
-def t_VAR(t):
-    r'[_A-Za-z][_0-9A-Za-z]*'
+def t_IGN(t):
+    r' \t\n'
+    return t
+
+def t_RET(t):
+    r'% *return'
+    return t
+
+def t_EOF(t):
+    r'$'
     return t
 
 lexer = lex.lex()
 lexer.forLEX = []
 lexer.forYACC = []
+
+import sys
+# cat text.txt | python3.10 ex2.py
+f = open("../input/example1.txt")
+for line in f:
+    lexer.input(line)
+print("\n\nEncontrei " + "\n".join(lexer.forLEX))
